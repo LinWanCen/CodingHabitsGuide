70c779e5bbc4b04d2bf86e0a30be3181cdc66c21
林万程 <1498425439@qq.com>
2021-08-03 11:36:05 +0800

    计算 JVM 所需内存脚本注释

--- java/java_sh/check_java_mem.sh
+++ java/java_sh/check_java_mem.sh
@@ -11,25 +11,43 @@ do
   INFO=`$J_INFO $PID`
   JVM_MEM=`echo "$INFO" | sed -n 's/.*MaxHeapSize=\([0-9]*\) .*/\1/p'`
   if [ 0"$JVM_MEM" = 0 ] ; then
+    # JDK 7 没有 MaxHeapSize，查设置
     JVM_MEM=`echo "$INFO" | sed -n 's/.*Xmx\([0-9]*\)[Mm].*/\1/p'`
     JVM_MEM=$((JVM_MEM * 1024 * 1024))
     if [ "$JVM_MEM" = 0 ] ; then
+      # GB 时转换不一样所以另外查
       JVM_MEM=`echo "$INFO" | sed -n 's/.*Xmx\([0-9]*\)[Gg].*/\1/p'`
       JVM_MEM=$((JVM_MEM * 1024 * 1024 * 1024))
     fi
+    # 若没有就用 JAVA 默认内存
     if [ "$JVM_MEM" = 0 ] ; then
       JVM_MEM=`$JAVA -XX:+PrintFlagsFinal -version | grep MaxHeapSize | sed -n 's/.*= \([0-9]*\) .*/\1/p'`
     fi
   fi
-  echo `echo "${JVM_MEM}" | awk '{print $1/1024/1024/1024" GB"}'` $J_INFO `echo "$JPS" | grep $PID`
+  # 显示时转换为 GB
+  echo `echo "${JVM_MEM}" | awk '{print $1/1024/1024/1024" G"}'` $J_INFO `echo "$JPS" | grep $PID`
+  # 单位是 b
   JVM_MEM_SUM=$((JVM_MEM_SUM+=JVM_MEM))
 done
-echo "`echo "${JVM_MEM_SUM}" | awk '{print $1/1024/1024/1024" GB"}'`"
 
+# 显示 jvm 内存总需求
+echo "`echo "${JVM_MEM_SUM}" | awk '{print $1/1024/1024/1024" G"}'`"
+
+# 机器内存默认 k 为单位，所以要 * 1024
 TOTAL_MEM=`free|grep Mem|awk '{print $2*1024}'`
+# 显示机器总内存
 echo TOTAL_MEM: "`echo "${TOTAL_MEM}" | awk '{print $1/1024/1024/1024" G"}'`"
 
+# 机器交换空间默认 k 为单位，所以要 * 1024
+TOTAL_SWAP=`free|grep Swap|awk '{print $2*1024}'`
+# 显示机器总交换空间
+echo TOTAL_SWAP: "`echo "${TOTAL_SWAP}" | awk '{print $1/1024/1024/1024" G"}'`"
+
 free -h
+
+if [ $JVM_MEM_SUM -ge $[$TOTAL_MEM + $TOTAL_SWAP] ]; then
+  exit 2
+fi
 
 if [ $JVM_MEM_SUM -ge $TOTAL_MEM ]; then
   exit 1
